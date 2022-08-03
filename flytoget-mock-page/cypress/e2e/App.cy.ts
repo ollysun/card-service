80915b1690eb3ef5685b1d0b279004ba844566b5
tunji <tunji.moronkola@vayapay.com>
2022-08-04 00:08:08 +0100

    Add few more test cases

--- flytoget-mock-page/cypress/e2e/App.cy.ts
+++ flytoget-mock-page/cypress/e2e/App.cy.ts
@@ -6,28 +6,36 @@ const getIframeContentBody = () => {
 
 describe('register profile', () => {
 
-    it('should contains iframe', () => {
+    beforeEach(() => {
         cy.visit('/')
+    })
+
+    it('should contains iframe', () => {
         cy.get('.App-iframe')
         cy.get('iframe')
     })
 
     it('iframe should contain content', function () {
-        cy.visit('/')
         console.log(getIframeContentBody())
         getIframeContentBody()
     });
 
-    it('iframe should contain some label', function () {
-        cy.visit('/')
+    it('iframe should contain some labels', function () {
         getIframeContentBody().contains('h6', 'Add Card')
         getIframeContentBody().contains('label', 'Card Number')
         getIframeContentBody().contains('label', 'Expiry Date')
+    });
+
+    it('iframe should contain save card button', function () {
         getIframeContentBody().contains('button', 'Save Card')
+    });
+
+    it('iframe should load a save card button', function () {
         getIframeContentBody().find('#saveCard').should('have.text', 'Save Card')
-        getIframeContentBody().find('#cardNumber').focus().type("32437848848448")
-        getIframeContentBody().find('#cardBanner')
-            .should('have.css', 'background-color','rgb(173, 194, 238)')
+    });
+
+    it('account number field should be disabled by default', function () {
+        getIframeContentBody().find('#accountNumberContainer').should('have.css', 'display', "none")
     });
 
 })
