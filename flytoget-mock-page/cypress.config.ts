cfbdf7d263d0ac0f112ecc2ef6532cf985332ff8
tunji <tunji.moronkola@vayapay.com>
2022-08-03 20:13:27 +0100

    Set cypress configuration for loading iframe

--- flytoget-mock-page/cypress.config.ts
+++ flytoget-mock-page/cypress.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  chromeWebSecurity : false,
+  retries: {
+    runMode: 5
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
