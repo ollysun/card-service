22c7f4b0a2a0cdff720af9907e4525aa8dfa84db
tunji <tunji.moronkola@vayapay.com>
2022-08-03 20:13:57 +0100

    Add data-cy attributes

--- flytoget-mock-page/src/components/Iframe.tsx
+++ flytoget-mock-page/src/components/Iframe.tsx
@@ -10,7 +10,7 @@ const Iframe: FC<IframeProps> = (props) : JSX.Element => {
     return (
         <Container className="px-3">
             <iframe className="App-iframe" title={props.title}
-                    src={props.src}/>
+                    src={props.src} data-cy="card-iframe"/>
         </Container>
     );
 }
