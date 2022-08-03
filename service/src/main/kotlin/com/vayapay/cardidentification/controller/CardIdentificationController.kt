1a05332a94b1863bd7b0ca488772c4d3c7626e35
moses olalere <ollysun@gmail.com>
2022-08-03 04:26:57 +0100

    work on controller

--- service/src/main/kotlin/com/vayapay/cardidentification/controller/CardIdentificationController.kt
+++ service/src/main/kotlin/com/vayapay/cardidentification/controller/CardIdentificationController.kt
@@ -1,5 +1,24 @@
 package com.vayapay.cardidentification.controller
 
+import com.vayapay.cardidentification.core.CardIdentificationService
+import com.vayapay.cardidentification.model.CardRequestDto
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import javax.validation.Valid
 
-class CardIdentificationController {
+
+@Controller
+@RequestMapping("/card-registration")
+class CardIdentificationController(
+    val cardIdentificationService: CardIdentificationService
+) {
+
+    @PostMapping
+    fun cardRegistration(@RequestBody @Valid cardRegistration: CardRequestDto): String? {
+        return null;
+    }
 }
