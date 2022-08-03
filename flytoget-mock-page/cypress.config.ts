62149d5cb23e58ef5735e8bdaa676c7a4174c5b0
tunji <tunji.moronkola@vayapay.com>
2022-08-03 22:57:59 +0100

    Iframe end-to-end testing

--- flytoget-mock-page/cypress.config.ts
+++ flytoget-mock-page/cypress.config.ts
@@ -2,13 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   chromeWebSecurity : false,
-  retries: {
-    runMode: 5
-  },
+
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: process.env.REACT_APP_BASE_URL || "http://localhost:3000",
   },
 });
--- flytoget-mock-page/cypress/e2e/App.cy.ts
+++ flytoget-mock-page/cypress/e2e/App.cy.ts
@@ -1,29 +1,33 @@
-describe('register profile', () => {
-  beforeEach( () => {
-    cy.visit('/')
-  })
-
-  it('should contains iframe', () => {
-    cy.get('.App-iframe')
-    cy.get('iframe')
-  })
-
- const getIframeContentBody = () => {
-    return cy.get('.App-iframe')
-        .its('0.contentDocument').should('exist')
-        .its('body').should('not.be.undefined')
+const getIframeContentBody = () => {
+    return cy.get('iframe[data-cy="card-iframe"]')
+        .its('0.contentDocument.body').should('not.be.empty')
         .then(cy.wrap)
-  }
+}
 
-  it('iframe should contain content', function () {
-    getIframeContentBody()
-  });
+describe('register profile', () => {
 
-  it('iframe should contain some label', function () {
-    getIframeContentBody().contains('h6', 'Add Card')
-    getIframeContentBody().contains('label', 'Card Number')
-    getIframeContentBody().contains('label', 'Expiry Date')
-    getIframeContentBody().contains('button', 'Save Card')
-  });
+    it('should contains iframe', () => {
+        cy.visit('/')
+        cy.get('.App-iframe')
+        cy.get('iframe')
+    })
+
+    it('iframe should contain content', function () {
+        cy.visit('/')
+        console.log(getIframeContentBody())
+        getIframeContentBody()
+    });
+
+    it('iframe should contain some label', function () {
+        cy.visit('/')
+        getIframeContentBody().contains('h6', 'Add Card')
+        getIframeContentBody().contains('label', 'Card Number')
+        getIframeContentBody().contains('label', 'Expiry Date')
+        getIframeContentBody().contains('button', 'Save Card')
+        getIframeContentBody().find('#saveCard').should('have.text', 'Save Card')
+        getIframeContentBody().find('#cardNumber').focus().type("32437848848448")
+        getIframeContentBody().find('#cardBanner')
+            .should('have.css', 'background-color','rgb(173, 194, 238)')
+    });
 
 })
