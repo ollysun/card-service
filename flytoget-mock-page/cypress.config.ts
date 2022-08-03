5136736544bf3eece938db45fafd43de0ab5bec5
tunji <tunji.moronkola@vayapay.com>
2022-08-03 12:38:55 +0100

    Configure base url on cypress

--- flytoget-mock-page/cypress.config.ts
+++ flytoget-mock-page/cypress.config.ts
@@ -5,5 +5,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: process.env.REACT_APP_BASE_URL || "http://localhost:3000",
   },
 });
--- flytoget-mock-page/cypress/e2e/registration.cy.ts
+++ flytoget-mock-page/cypress/e2e/registration.cy.ts
@@ -1,5 +1,5 @@
 describe('empty spec', () => {
   it('passes', () => {
-    cy.visit('localhost:3000')
+    cy.visit('/')
   })
 })
