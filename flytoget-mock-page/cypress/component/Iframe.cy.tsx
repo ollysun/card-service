b190fe6eb31033a5d421f8d86736568780104b63
tunji <tunji.moronkola@vayapay.com>
2022-08-05 12:00:04 +0100

    Add component test

---
+++ flytoget-mock-page/cypress/component/Iframe.cy.tsx
@@ -0,0 +1,23 @@
+import Iframe from "../../src/components/Iframe";
+
+describe('Iframe.cy.ts', () => {
+
+    const title = process.env.REACT_APP_IFRAME_TITLE || "card registration iframe"
+    const src = process.env.REACT_APP_IFRAME_URL || " http://localhost:8000"
+
+  it('playground', () => {
+    cy.mount(<Iframe title={title} src={src} />);
+  });
+
+    it("should load card registration content", () => {
+        cy.mount(<Iframe title={title} src={src} />)
+        cy.get('iframe[data-cy="card-iframe"]')
+            .its('0.contentDocument.body').should('not.be.empty')
+    })
+
+    it("should contains Add card test", () => {
+        cy.mount(<Iframe title={title} src={src} />)
+        cy.get('iframe[data-cy="card-iframe"]')
+            .its('0.contentDocument.body').contains("Add Card")
+    })
+})
