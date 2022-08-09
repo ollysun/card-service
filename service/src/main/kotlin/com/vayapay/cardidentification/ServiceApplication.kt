cc14eba0ba31acd8fdf87d0b6af6a9b56e9b46d0
moses olalere <ollysun@gmail.com>
2022-08-09 23:56:30 +0100

    remove unused import

--- service/src/main/kotlin/com/vayapay/cardidentification/ServiceApplication.kt
+++ service/src/main/kotlin/com/vayapay/cardidentification/ServiceApplication.kt
@@ -1,7 +1,6 @@
 package com.vayapay.cardidentification
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
