dfc62565565c48ddb224bc7558b4f5e60785e68a
tunji <tunji.moronkola@vayapay.com>
2022-08-15 15:45:06 +0100

    Build Select option utils

---
+++ service/src/main/kotlin/com/vayapay/cardidentification/utils/SelectOptions.kt
@@ -0,0 +1,36 @@
+package com.vayapay.cardidentification.utils
+
+import java.time.LocalDate
+
+object SelectOptions {
+
+    val MONTHS = listOf<String>(
+       "01",
+       "02",
+       "03",
+       "04",
+       "05",
+       "06",
+       "07",
+       "08",
+       "09",
+       "10",
+       "11",
+       "12"
+    )
+
+   private fun getYears(): List<String> {
+       val maxYear = LocalDate.now().year.plus(10)
+       val minYear = maxYear - 13
+
+       val years = mutableListOf<String>()
+
+       for(year in minYear..maxYear)
+           years.add(year.toString())
+
+       return years
+   }
+
+    val  YEARS = getYears()
+
+}
