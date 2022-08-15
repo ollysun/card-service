9f30663a58d8947efa2b892d928d63e45e44a6e2
tunji <tunji.moronkola@vayapay.com>
2022-08-15 09:39:01 +0100

    Optimize the card controller

--- service/src/main/kotlin/com/vayapay/cardidentification/controller/CardController.kt
+++ service/src/main/kotlin/com/vayapay/cardidentification/controller/CardController.kt
@@ -1,13 +1,9 @@
 package com.vayapay.cardidentification.controller
 
-import com.vayapay.cardidentification.model.CardRequestDto
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import javax.validation.Valid
 
 
 @Controller
