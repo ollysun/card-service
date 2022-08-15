860244cfcdd3c6f1d1f852c108c8b5a840141f52
tunji <tunji.moronkola@vayapay.com>
2022-08-15 15:46:11 +0100

    Add select option to controller model

--- service/src/main/kotlin/com/vayapay/cardidentification/controller/CardController.kt
+++ service/src/main/kotlin/com/vayapay/cardidentification/controller/CardController.kt
@@ -1,5 +1,6 @@
 package com.vayapay.cardidentification.controller
 
+import com.vayapay.cardidentification.utils.SelectOptions
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,6 +13,7 @@ class CardController {
 
     @GetMapping("")
     fun addCard(model: Model): String {
+        model.addAttribute("options", SelectOptions)
         return "add-card"
     }
 }
