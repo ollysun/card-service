79a80fb784f611f45d4390168a24e5663afdb522
tunji <tunji.moronkola@vayapay.com>
2022-08-21 11:52:28 +0100

    Update year drop down optiojn

--- service/src/main/kotlin/com/vayapay/cardidentification/utils/SelectOptions.kt
+++ service/src/main/kotlin/com/vayapay/cardidentification/utils/SelectOptions.kt
@@ -5,32 +5,32 @@ import java.time.LocalDate
 object SelectOptions {
 
     val MONTHS = listOf<String>(
-       "01",
-       "02",
-       "03",
-       "04",
-       "05",
-       "06",
-       "07",
-       "08",
-       "09",
-       "10",
-       "11",
-       "12"
+        "01",
+        "02",
+        "03",
+        "04",
+        "05",
+        "06",
+        "07",
+        "08",
+        "09",
+        "10",
+        "11",
+        "12"
     )
 
-   private fun getYears(): List<String> {
-       val maxYear = LocalDate.now().year.plus(10)
-       val minYear = maxYear - 13
+    private fun getYears(): List<String> {
+        val maxYear = LocalDate.now().year.plus(10)
+        val minYear = maxYear - 13
 
-       val years = mutableListOf<String>()
+        val years = mutableListOf<String>()
 
-       for(year in minYear..maxYear)
-           years.add(year.toString())
+        for (year in minYear..maxYear)
+            years.add((year - 2000).toString())
 
-       return years
-   }
+        return years
+    }
 
-    val  YEARS = getYears()
+    val YEARS = getYears()
 
 }
