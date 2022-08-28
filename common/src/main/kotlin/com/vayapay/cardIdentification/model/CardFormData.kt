be72c2cb2dd25f084abebb13600c9c99e4fd5605
tunji <tunji.moronkola@vayapay.com>
2022-08-28 22:21:08 +0100

    Change some validation messages

--- common/src/main/kotlin/com/vayapay/cardIdentification/model/CardFormData.kt
+++ common/src/main/kotlin/com/vayapay/cardIdentification/model/CardFormData.kt
@@ -10,10 +10,10 @@ class CardFormData(
     @Size(min = 13, max = 19, message = "Card number must be of length between 13 and 19")
     val cardNumber: String,
     @NotEmpty(message = "Expiry month is mandatory")
-    @Size(min = 2, max = 2, message = "expiry month must be of length 2")
+    @Size(min = 2, max = 2, message = "Expiry month must be of length 2")
     val expiryMonth: String,
-    @NotEmpty(message = "expiry year is mandatory")
-    @Size(min = 2, max = 2, message = "expiry year must be of length 2")
+    @NotEmpty(message = "Expiry year is mandatory")
+    @Size(min = 2, max = 2, message = "Expiry year must be of length 2")
     val expiryYear: String,
     @Size(max = 11, message = "Account number must not be more than 11")
     val accountNumber: String? = null
