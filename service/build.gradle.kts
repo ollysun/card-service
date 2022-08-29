b5a2b6f81d9a43b6ddf605d01f1a935e9d0a6d21
moses olalere <ollysun@gmail.com>
2022-08-29 17:00:56 +0100

    remove unnecessary file

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -2,14 +2,12 @@ val commonUtilsVersion: String by project
 val hsmProxyVersion: String by project
 val jUnit: String by project
 val logbackEncoder: String by project
-val r2dbcVersion: String by project
 val berTlvVersion: String by project
 val springMockkVersion: String by project
 val kotlinVersion: String by project
 val shedLockVersion: String by project
 val loggingUtilsVersion: String by project
 val awSpringVersion: String by project
-
 plugins {
 	id("org.springframework.boot")
 	kotlin("plugin.spring")
@@ -39,26 +37,18 @@ dependencies {
 
 	}
 
-	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-cache")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 
 	implementation("com.github.ben-manes.caffeine:caffeine")
-	implementation(platform("io.r2dbc:r2dbc-bom:$r2dbcVersion"))
-	implementation("io.r2dbc:r2dbc-spi")
-	implementation("io.r2dbc:r2dbc-postgresql") {
-		exclude(group = "io.netty.incubator", module = "netty-incubator-codec-classes-quic")
-		exclude(group = "io.netty.incubator", module = "netty-incubator-codec-native-quic")
-	}
+
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j")
 	implementation("com.payneteasy:ber-tlv:$berTlvVersion")
 	implementation("com.vayapay.common:common-utils:$commonUtilsVersion")
 	implementation("com.vayapay.hsmproxy:hsm-proxy-client:$hsmProxyVersion")
 	implementation("com.vayapay.hsmproxy:hsm-proxy-common:$hsmProxyVersion")
 	implementation("net.javacrumbs.shedlock:shedlock-spring:$shedLockVersion")
-	implementation("net.javacrumbs.shedlock:shedlock-provider-r2dbc:$shedLockVersion")
-	implementation ("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
 
 	runtimeOnly("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
 	runtimeOnly("org.flywaydb:flyway-core")
@@ -71,7 +61,6 @@ dependencies {
 	runtimeOnly("org.codehaus.janino:janino")
 	runtimeOnly("com.vayapay.common:logging-utils:$loggingUtilsVersion")
 	runtimeOnly("io.awspring.cloud:spring-cloud-starter-aws-secrets-manager-config:$awSpringVersion")
-	testImplementation("io.r2dbc:r2dbc-h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("com.ninja-squad:springmockk:$springMockkVersion")
 	//testImplementation(project(":card-data-service-client"))
