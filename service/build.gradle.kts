cbf966e271603134bf8c5fe2b92bed125ff4ad18
tunji <tunji.moronkola@vayapay.com>
2022-08-29 21:23:23 +0100

    remove unused webflux dependency

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -24,7 +24,6 @@ dependencies {
         because("CVE-2020-36518")
     }
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-devtools")
