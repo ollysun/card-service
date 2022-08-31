8bb6a9112d9bdf6c6eaf43ac090a80307f731fa5
tunji <tunji.moronkola@vayapay.com>
2022-09-01 00:32:35 +0100

    Account number check

--- common/src/main/kotlin/com/vayapay/cardIdentification/model/AddCardForm.kt
+++ common/src/main/kotlin/com/vayapay/cardIdentification/model/AddCardForm.kt
@@ -2,7 +2,6 @@ package com.vayapay.cardIdentification.model
 
 import kotlinx.serialization.Serializable
 import org.hibernate.validator.constraints.CreditCardNumber
-import org.hibernate.validator.constraints.Mod11Check
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.Size
 
@@ -12,13 +11,15 @@ class AddCardForm(
     @field:NotEmpty(message = "Card number is mandatory")
     @field:Size(min = 13, max = 19, message = "Card number must be between 13 and 19 digits")
     val cardNumber: String,
+
     @field:NotEmpty(message = "Expiry month is mandatory")
     @field:Size(min = 2, max = 2, message = "Expiry month must be of length 2")
     val expiryMonth: String,
+
     @field:NotEmpty(message = "Expiry year is mandatory")
     @field:Size(min = 2, max = 2, message = "Expiry year must be of length 2")
     val expiryYear: String,
-    @field:Size(min = 11, max = 11, message = "Account number must be of length 11")
-    @field:Mod11Check
+
+    @field:Size(max = 11, message = "Account number must not be more than 11 digits")
     val accountNumber: String? = null
 )
