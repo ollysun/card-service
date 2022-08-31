f3e5ad6e8c90ee19e6291797acacaa4711f4735d
tunji <tunji.moronkola@vayapay.com>
2022-08-31 23:00:53 +0100

    fix backend validation issue

--- common/src/main/kotlin/com/vayapay/cardIdentification/model/AddCardForm.kt
+++ common/src/main/kotlin/com/vayapay/cardIdentification/model/AddCardForm.kt
@@ -1,20 +1,24 @@
 package com.vayapay.cardIdentification.model
 
 import kotlinx.serialization.Serializable
+import org.hibernate.validator.constraints.CreditCardNumber
+import org.hibernate.validator.constraints.Mod11Check
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.Size
 
 @Serializable
 class AddCardForm(
-    @NotEmpty(message = "Card number is mandatory")
-    @Size(min = 13, max = 19, message = "Card number must be of length between 13 and 19")
+    @field:CreditCardNumber
+    @field:NotEmpty(message = "Card number is mandatory")
+    @field:Size(min = 13, max = 19, message = "Card number must be between 13 and 19 digits")
     val cardNumber: String,
-    @NotEmpty(message = "Expiry month is mandatory")
-    @Size(min = 2, max = 2, message = "Expiry month must be of length 2")
+    @field:NotEmpty(message = "Expiry month is mandatory")
+    @field:Size(min = 2, max = 2, message = "Expiry month must be of length 2")
     val expiryMonth: String,
-    @NotEmpty(message = "Expiry year is mandatory")
-    @Size(min = 2, max = 2, message = "Expiry year must be of length 2")
+    @field:NotEmpty(message = "Expiry year is mandatory")
+    @field:Size(min = 2, max = 2, message = "Expiry year must be of length 2")
     val expiryYear: String,
-    @Size(min = 11, max = 11, message = "Account number must be of length 11")
+    @field:Size(min = 11, max = 11, message = "Account number must be of length 11")
+    @field:Mod11Check
     val accountNumber: String? = null
 )
