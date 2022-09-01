73af23b9f965df7ab8ada1772602fd345ef37724
tunji <tunji.moronkola@vayapay.com>
2022-09-01 15:19:19 +0100

    Some test script

--- service/src/test/kotlin/com/vayapay/cardIdentification/controller/CardIdentificationControllerTest.kt
+++ service/src/test/kotlin/com/vayapay/cardIdentification/controller/CardIdentificationControllerTest.kt
@@ -1,33 +1,57 @@
 package com.vayapay.cardIdentification.controller
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.ninjasquad.springmockk.MockkBean
+import com.vayapay.cardIdentification.config.WebSecurityConfig
 import com.vayapay.cardIdentification.core.CardIdentificationService
-import com.vayapay.cardIdentification.model.AddCardForm
+import com.vayapay.cardIdentification.model.*
+import io.mockk.coEvery
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.context.annotation.Import
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.util.LinkedMultiValueMap
 import org.springframework.web.context.WebApplicationContext
+import java.util.*
 
 private const val ADD_CARD_URL = "/card-registration"
 
-@ExtendWith(SpringExtension::class)
-@ContextConfiguration()
+private const val VALID_CARD_NUMBER = "5555555555555554444"
+
+private const val INVALID_CARD_NUMBER = ""
+
+private const val VALID_EXPIRY_MONTH = "09"
+private const val INVALID_EXPIRY_MONTH = "0"
+
+private const val VALID_EXPIRY_YEAR = "24"
+private const val INVALID_EXPIRY_YEAR = "24"
+
+private const val CARD_NUMBER_PARAM = "cardNumber"
+
+private const val EXPIRY_MONTH_PARAM = "expiryMonth"
+
+private const val EXPIRY_YEAR_PARAM = "expiryYear"
+
+private const val PTO_ID = "PTO_1"
+
+private const val ACCOUNT_NUMBER = "1234567890"
+
+private const val MASTER_SCHEME = "Master"
+
+
+@ContextConfiguration
 @WebMvcTest(CardIdentificationController::class)
-@WithMockUser
+@Import(WebSecurityConfig::class)
 class CardIdentificationControllerTest {
 
     @Autowired
@@ -35,16 +59,55 @@ class CardIdentificationControllerTest {
 
     private lateinit var mockMvc: MockMvc
 
+    private lateinit var addCardForm: AddCardForm
+
     @MockkBean
-    private lateinit var cardService: CardIdentificationService
+    private lateinit var objectMapper: ObjectMapper
+
+    @MockkBean
+    private lateinit var cardIdentificationService: CardIdentificationService
+
+    private var validTestCard = LinkedMultiValueMap<String, String>()
+    private var invalidTestCard = LinkedMultiValueMap<String, String>()
+
+    private fun createTestCard(
+        cardNumber: String,
+        expiryMonth: String,
+        expiryYear: String
+    ): LinkedMultiValueMap<String, String> {
+        val testCard = LinkedMultiValueMap<String, String>()
+        testCard[CARD_NUMBER_PARAM] = cardNumber
+        testCard[EXPIRY_MONTH_PARAM] = expiryMonth
+        testCard[EXPIRY_YEAR_PARAM] = expiryYear
+        return testCard
+    }
+
+    private fun buildCardRequestDto(): CardRequestDto {
+        val cardData = CardData(VALID_CARD_NUMBER, VALID_EXPIRY_MONTH + VALID_EXPIRY_YEAR)
+        return CardRequestDto(PTO_ID, cardData, ACCOUNT_NUMBER)
+    }
+
+    private fun buildValidSaveCardResponse(): StoreCardResponse {
+        val cardIdResponse = CardIdResponse(MASTER_SCHEME, UUID.randomUUID(), UUID.randomUUID())
+        return StoreCardResponse(cardIdResponse, "", "")
+    }
+
+    private fun buildInvalidSaveCardResponse(): StoreCardResponse {
+        val cardIdResponse = CardIdResponse("", UUID.randomUUID(), UUID.randomUUID())
+        return StoreCardResponse(cardIdResponse, "Fail", "Invalid request")
+    }
 
     @BeforeEach
     fun setup() {
         runBlocking {
             mockMvc = MockMvcBuilders
                 .webAppContextSetup(context)
-                .apply<DefaultMockMvcBuilder>(springSecurity())
+                .apply<DefaultMockMvcBuilder>(SecurityMockMvcConfigurers.springSecurity())
                 .build()
+
+            validTestCard = createTestCard(VALID_CARD_NUMBER, VALID_EXPIRY_MONTH, VALID_EXPIRY_YEAR)
+            invalidTestCard = createTestCard(INVALID_CARD_NUMBER, INVALID_EXPIRY_MONTH, INVALID_EXPIRY_YEAR)
+
         }
     }
 
@@ -54,26 +117,52 @@ class CardIdentificationControllerTest {
             mockMvc.get(ADD_CARD_URL)
                 .andExpect {
                     status { isOk() }
-                    content {
-                        string("")
-                    }
+                    view { ADD_CARD_URL }
                 }
         }
     }
 
     @Test
-    fun `should post addCardForm`() {
+    fun `should fail to add card without csrf with Forbidden error`() {
+        mockMvc.post(ADD_CARD_URL) {
+            params = validTestCard
+        }.andExpect {
+            status {
+                is4xxClientError()
+                isForbidden()
+            }
+            view { ADD_CARD_URL }
+        }
+    }
+
+    @Test
+    fun `should add card`() {
         runBlocking {
-            val addCardForm = AddCardForm("378282246310005", "09", "24")
+            coEvery { cardIdentificationService.saveCardStorage(buildCardRequestDto()) } returns buildValidSaveCardResponse()
+
             mockMvc.post(ADD_CARD_URL) {
-                content = jacksonObjectMapper().writeValueAsString(addCardForm)
+                params = validTestCard
                 with(csrf())
             }.andExpect {
                 status { isOk() }
-                content {
-                    string("")
-                }
+                view { ADD_CARD_URL }
             }
         }
     }
+
+    @Test
+    fun `should fail to add card data`() {
+        coEvery { cardIdentificationService.saveCardStorage(buildCardRequestDto()) } returns buildInvalidSaveCardResponse()
+        mockMvc.post(ADD_CARD_URL) {
+            params = invalidTestCard
+            with(csrf())
+        }.andExpect {
+            status {
+                isOk()
+            }
+            view { ADD_CARD_URL }
+        }
+    }
+
+
 }
