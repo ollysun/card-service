fbe04f53a845518286394bf8e65d3601b0944c7e
tunji <tunji.moronkola@vayapay.com>
2022-09-01 17:46:42 +0100

    testing htmlunit

--- service/src/test/kotlin/com/vayapay/cardIdentification/controller/CardIdentificationControllerTest.kt
+++ service/src/test/kotlin/com/vayapay/cardIdentification/controller/CardIdentificationControllerTest.kt
@@ -1,11 +1,14 @@
 package com.vayapay.cardIdentification.controller
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.ninjasquad.springmockk.MockkBean
+import com.gargoylesoftware.htmlunit.WebClient
 import com.vayapay.cardIdentification.config.WebSecurityConfig
 import com.vayapay.cardIdentification.core.CardIdentificationService
-import com.vayapay.cardIdentification.model.*
+import com.vayapay.cardIdentification.model.CardData
+import com.vayapay.cardIdentification.model.CardIdResponse
+import com.vayapay.cardIdentification.model.CardRequestDto
+import com.vayapay.cardIdentification.model.StoreCardResponse
 import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -13,12 +16,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.context.annotation.Import
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.htmlunit.MockMvcWebClientBuilder
 import org.springframework.test.web.servlet.post
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.web.context.WebApplicationContext
@@ -59,55 +61,27 @@ class CardIdentificationControllerTest {
 
     private lateinit var mockMvc: MockMvc
 
-    private lateinit var addCardForm: AddCardForm
+    private lateinit var webClient: WebClient
 
-    @MockkBean
-    private lateinit var objectMapper: ObjectMapper
-
-    @MockkBean
-    private lateinit var cardIdentificationService: CardIdentificationService
+    private val cardIdentificationService = mockk<CardIdentificationService>()
 
     private var validTestCard = LinkedMultiValueMap<String, String>()
+
     private var invalidTestCard = LinkedMultiValueMap<String, String>()
-
-    private fun createTestCard(
-        cardNumber: String,
-        expiryMonth: String,
-        expiryYear: String
-    ): LinkedMultiValueMap<String, String> {
-        val testCard = LinkedMultiValueMap<String, String>()
-        testCard[CARD_NUMBER_PARAM] = cardNumber
-        testCard[EXPIRY_MONTH_PARAM] = expiryMonth
-        testCard[EXPIRY_YEAR_PARAM] = expiryYear
-        return testCard
-    }
-
-    private fun buildCardRequestDto(): CardRequestDto {
-        val cardData = CardData(VALID_CARD_NUMBER, VALID_EXPIRY_MONTH + VALID_EXPIRY_YEAR)
-        return CardRequestDto(PTO_ID, cardData, ACCOUNT_NUMBER)
-    }
-
-    private fun buildValidSaveCardResponse(): StoreCardResponse {
-        val cardIdResponse = CardIdResponse(MASTER_SCHEME, UUID.randomUUID(), UUID.randomUUID())
-        return StoreCardResponse(cardIdResponse, "", "")
-    }
-
-    private fun buildInvalidSaveCardResponse(): StoreCardResponse {
-        val cardIdResponse = CardIdResponse("", UUID.randomUUID(), UUID.randomUUID())
-        return StoreCardResponse(cardIdResponse, "Fail", "Invalid request")
-    }
 
     @BeforeEach
     fun setup() {
         runBlocking {
             mockMvc = MockMvcBuilders
                 .webAppContextSetup(context)
-                .apply<DefaultMockMvcBuilder>(SecurityMockMvcConfigurers.springSecurity())
+                .build()
+
+            webClient = MockMvcWebClientBuilder
+                .mockMvcSetup(mockMvc)
                 .build()
 
             validTestCard = createTestCard(VALID_CARD_NUMBER, VALID_EXPIRY_MONTH, VALID_EXPIRY_YEAR)
             invalidTestCard = createTestCard(INVALID_CARD_NUMBER, INVALID_EXPIRY_MONTH, INVALID_EXPIRY_YEAR)
-
         }
     }
 
@@ -162,6 +136,39 @@ class CardIdentificationControllerTest {
             }
             view { ADD_CARD_URL }
         }
+    }
+
+    @Test
+    fun `s`() {
+
+
+    }
+
+    private fun createTestCard(
+        cardNumber: String,
+        expiryMonth: String,
+        expiryYear: String
+    ): LinkedMultiValueMap<String, String> {
+        val testCard = LinkedMultiValueMap<String, String>()
+        testCard[CARD_NUMBER_PARAM] = cardNumber
+        testCard[EXPIRY_MONTH_PARAM] = expiryMonth
+        testCard[EXPIRY_YEAR_PARAM] = expiryYear
+        return testCard
+    }
+
+    private fun buildCardRequestDto(): CardRequestDto {
+        val cardData = CardData(VALID_CARD_NUMBER, VALID_EXPIRY_MONTH + VALID_EXPIRY_YEAR)
+        return CardRequestDto(PTO_ID, cardData, ACCOUNT_NUMBER)
+    }
+
+    private fun buildValidSaveCardResponse(): StoreCardResponse {
+        val cardIdResponse = CardIdResponse(MASTER_SCHEME, UUID.randomUUID(), UUID.randomUUID())
+        return StoreCardResponse(cardIdResponse, "", "")
+    }
+
+    private fun buildInvalidSaveCardResponse(): StoreCardResponse {
+        val cardIdResponse = CardIdResponse("", UUID.randomUUID(), UUID.randomUUID())
+        return StoreCardResponse(cardIdResponse, "Fail", "Invalid request")
     }
 
 
