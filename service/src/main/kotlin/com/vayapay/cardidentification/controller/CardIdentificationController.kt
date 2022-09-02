f0708a122b73d2dea2384138d341f432dfa11d37
moses olalere <ollysun@gmail.com>
2022-09-02 12:06:52 +0100

    controller

--- service/src/main/kotlin/com/vayapay/cardidentification/controller/CardIdentificationController.kt
+++ service/src/main/kotlin/com/vayapay/cardidentification/controller/CardIdentificationController.kt
@@ -3,7 +3,6 @@ package com.vayapay.cardidentification.controller
 import com.vayapay.carddata.messages.StoreAndLinkCardDataResponse
 import com.vayapay.cardidentification.core.CardIdentificationService
 import com.vayapay.cardidentification.model.CardRequestDto
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
