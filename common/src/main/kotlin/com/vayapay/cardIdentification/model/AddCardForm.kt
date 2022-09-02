8657511cf1ef230e6413ef7fb91119f31b76b48b
tunji <tunji.moronkola@vayapay.com>
2022-09-02 16:04:03 +0100

    Enable Iframe options

--- common/src/main/kotlin/com/vayapay/cardIdentification/model/AddCardForm.kt
+++ common/src/main/kotlin/com/vayapay/cardIdentification/model/AddCardForm.kt
@@ -6,7 +6,7 @@ import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.Size
 
 @Serializable
-class AddCardForm(
+data class AddCardForm(
     @field:CreditCardNumber
     @field:NotEmpty(message = "Card number is mandatory")
     @field:Size(min = 13, max = 19, message = "Card number must be between 13 and 19 digits")
